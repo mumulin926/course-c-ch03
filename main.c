c53ec3047f87b806a53b0541cfa10b91756d8027
mumulin926 <115057490+mumulin926@users.noreply.github.com>
2022-10-05 11:51:38 +0800

    practise-switch

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	int grade;
+	int a;
+	scanf("%d", &grade);
+	a = grade/10;
+	switch(a){
+		case 10:
+		case 9:
+			printf("A\n");
+			break;
+		case 8:
+			printf("B\n");
+			break;
+		case 7:
+			printf("C\n");
+			break;
+		case 6:
+			printf("D\n");
+			break;
+		default:
+			printf("E\n");
+			break;
+	} 
+	return 0;
+}
+
